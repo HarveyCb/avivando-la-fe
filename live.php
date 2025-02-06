1e981d0297d66c3f34a73bfbee40084bf661dd54
Harvey-MSI <chenbolh@gmail.com>
2025-02-06 10:39:49 -0600

    live

---
+++ live.php
@@ -0,0 +1,292 @@
+<!DOCTYPE html>
+<html lang="en" itemscope itemtype="http://schema.org/WebPage">
+  <head>
+    <meta property="og:title" content="Iglesia Avivando la Fe - Cobán" />
+    <meta property="og:description" content="Avivando la Fe Cobán " />
+    <meta property="og:image"
+      content="https://avivando-la-fe-coban.sotecprotech.com/assets/images/iglesia.jpg" />
+    <meta property="og:url"
+      content="https://avivando-la-fe-coban.sotecprotech.com/" />
+    <meta property="og:type" content="website" />
+    <meta property="og:site_name" content="Avivando la Fe Cobán" />
+    <meta charset="utf-8" />
+    <meta name="viewport"
+      content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <link rel="apple-touch-icon" sizes="76x76" href="./assets/img/icon.png">
+    <link rel="icon" type="image/png" href="./assets/img/icon.png">
+    <title>
+      inicio
+    </title>
+    <!--     Fonts and icons     -->
+    <link
+      href="https://fonts.googleapis.com/css?family=Open+Sans:300,400,600,700"
+      rel="stylesheet" />
+    <!-- Nucleo Icons -->
+    <link href="./assets/css/nucleo-icons.css" rel="stylesheet" />
+    <link href="./assets/css/nucleo-svg.css" rel="stylesheet" />
+    <!-- Font Awesome Icons -->
+    <script src="https://kit.fontawesome.com/42d5adcbca.js"
+      crossorigin="anonymous"></script>
+    <link href="./assets/css/nucleo-svg.css" rel="stylesheet" />
+    <!-- CSS Files -->
+    <link id="pagestyle" href="./assets/css/soft-design-system.css?v=1.0.9"
+      rel="stylesheet" />
+    <!-- Nepcha Analytics (nepcha.com) -->
+    <!-- Nepcha is a easy-to-use web analytics. No cookies and fully compliant with GDPR, CCPA and PECR. -->
+    <script defer data-site="YOUR_DOMAIN_HERE"
+      src="https://api.nepcha.com/js/nepcha-analytics.js"></script>
+  </head>
+
+  <body class="about-us">
+    <!-- Navbar Transparent -->
+    <nav
+      class="navbar navbar-expand-lg position-absolute top-0 z-index-3 w-100 shadow-none my-3  navbar-transparent ">
+      <div class="container">
+        <a class="navbar-brand  text-white " href="#" rel="tooltip"
+          title="Designed and Coded by Creative Tim" data-placement="bottom"
+          target="_blank">
+          Avivando la fe
+        </a>
+        <button class="navbar-toggler shadow-none ms-2" type="button"
+          data-bs-toggle="collapse" data-bs-target="#navigation"
+          aria-controls="navigation" aria-expanded="false"
+          aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon mt-2">
+            <span class="navbar-toggler-bar bar1"></span>
+            <span class="navbar-toggler-bar bar2"></span>
+            <span class="navbar-toggler-bar bar3"></span>
+          </span>
+        </button>
+        <div
+          class="collapse navbar-collapse w-100 pt-3 pb-2 py-lg-0 ms-lg-12 ps-lg-5"
+          id="navigation">
+          <ul class="navbar-nav navbar-nav-hover w-100">
+            <li class="nav-item dropdown dropdown-hover mx-2 ms-lg-6">
+              <a
+                class="nav-link ps-2 d-flex justify-content-between cursor-pointer align-items-center"
+                id="dropdownMenuPages8" data-bs-toggle="dropdown"
+                aria-expanded="false">
+                Pages.
+                <img src="./assets/img/down-arrow-white.svg" alt="down-arrow"
+                  class="arrow ms-1 d-lg-block d-none">
+                <img src="./assets/img/down-arrow-dark.svg" alt="down-arrow"
+                  class="arrow ms-1 d-lg-none d-block">
+              </a>
+              <div
+                class="dropdown-menu dropdown-menu-animation dropdown-md p-3 border-radius-lg mt-0 mt-lg-3"
+                aria-labelledby="dropdownMenuPages8">
+                <div class="d-none d-lg-block">
+
+                  <div
+                    class="dropdown-header text-dark font-weight-bolder d-flex justify-content-cente align-items-center px-0 mt-3">
+                    <div class="d-inline-block">
+                      <div
+                        class="icon icon-shape icon-xs border-radius-md bg-gradient-primary text-center me-2 d-flex align-items-center justify-content-center ps-0">
+                        <svg width="12px" height="12px" viewBox="0 0 42 42"
+                          version="1.1" xmlns="http://www.w3.org/2000/svg"
+                          xmlns:xlink="http://www.w3.org/1999/xlink">
+                          <title>office</title>
+                          <g stroke="none" stroke-width="1" fill="none"
+                            fill-rule="evenodd">
+                            <g transform="translate(-1869.000000, -293.000000)"
+                              fill="#FFFFFF" fill-rule="nonzero">
+                              <g transform="translate(1716.000000, 291.000000)">
+                                <g transform="translate(153.000000, 2.000000)">
+                                  <path
+                                    d="M12.25,17.5 L8.75,17.5 L8.75,1.75 C8.75,0.78225 9.53225,0 10.5,0 L31.5,0 C32.46775,0 33.25,0.78225 33.25,1.75 L33.25,12.25 L29.75,12.25 L29.75,3.5 L12.25,3.5 L12.25,17.5 Z"
+                                    opacity="0.6"></path>
+                                  <path
+                                    d="M40.25,14 L24.5,14 C23.53225,14 22.75,14.78225 22.75,15.75 L22.75,38.5 L19.25,38.5 L19.25,22.75 C19.25,21.78225 18.46775,21 17.5,21 L1.75,21 C0.78225,21 0,21.78225 0,22.75 L0,40.25 C0,41.21775 0.78225,42 1.75,42 L40.25,42 C41.21775,42 42,41.21775 42,40.25 L42,15.75 C42,14.78225 41.21775,14 40.25,14 Z M12.25,36.75 L7,36.75 L7,33.25 L12.25,33.25 L12.25,36.75 Z M12.25,29.75 L7,29.75 L7,26.25 L12.25,26.25 L12.25,29.75 Z M35,36.75 L29.75,36.75 L29.75,33.25 L35,33.25 L35,36.75 Z M35,29.75 L29.75,29.75 L29.75,26.25 L35,26.25 L35,29.75 Z M35,22.75 L29.75,22.75 L29.75,19.25 L35,19.25 L35,22.75 Z"></path>
+                                </g>
+                              </g>
+                            </g>
+                          </g>
+                        </svg>
+                      </div>
+                    </div>
+                    opciones
+                  </div>
+                  <a href="index.html" class="dropdown-item border-radius-md">
+                    <span class="ps-3">home</span>
+                  </a>
+                </div>
+                <div class="d-lg-none">
+
+                  <div
+                    class="dropdown-header text-dark font-weight-bolder d-flex justify-content-cente align-items-center px-0 mt-3">
+                    <div class="d-inline-block">
+                      <div
+                        class="icon icon-shape icon-xs border-radius-md bg-gradient-primary text-center me-2 d-flex align-items-center justify-content-center ps-0">
+                        <svg width="12px" height="12px" viewBox="0 0 42 42"
+                          version="1.1" xmlns="http://www.w3.org/2000/svg"
+                          xmlns:xlink="http://www.w3.org/1999/xlink">
+                          <title>office</title>
+                          <g stroke="none" stroke-width="1" fill="none"
+                            fill-rule="evenodd">
+                            <g transform="translate(-1869.000000, -293.000000)"
+                              fill="#FFFFFF" fill-rule="nonzero">
+                              <g transform="translate(1716.000000, 291.000000)">
+                                <g transform="translate(153.000000, 2.000000)">
+                                  <path
+                                    d="M12.25,17.5 L8.75,17.5 L8.75,1.75 C8.75,0.78225 9.53225,0 10.5,0 L31.5,0 C32.46775,0 33.25,0.78225 33.25,1.75 L33.25,12.25 L29.75,12.25 L29.75,3.5 L12.25,3.5 L12.25,17.5 Z"
+                                    opacity="0.6"></path>
+                                  <path
+                                    d="M40.25,14 L24.5,14 C23.53225,14 22.75,14.78225 22.75,15.75 L22.75,38.5 L19.25,38.5 L19.25,22.75 C19.25,21.78225 18.46775,21 17.5,21 L1.75,21 C0.78225,21 0,21.78225 0,22.75 L0,40.25 C0,41.21775 0.78225,42 1.75,42 L40.25,42 C41.21775,42 42,41.21775 42,40.25 L42,15.75 C42,14.78225 41.21775,14 40.25,14 Z M12.25,36.75 L7,36.75 L7,33.25 L12.25,33.25 L12.25,36.75 Z M12.25,29.75 L7,29.75 L7,26.25 L12.25,26.25 L12.25,29.75 Z M35,36.75 L29.75,36.75 L29.75,33.25 L35,33.25 L35,36.75 Z M35,29.75 L29.75,29.75 L29.75,26.25 L35,26.25 L35,29.75 Z M35,22.75 L29.75,22.75 L29.75,19.25 L35,19.25 L35,22.75 Z"></path>
+                                </g>
+                              </g>
+                            </g>
+                          </g>
+                        </svg>
+                      </div>
+                    </div>
+                    opciones
+                  </div>
+                  <a href="index.html" class="dropdown-item border-radius-md">
+                    <span class="ps-3">Home</span>
+                  </a>
+                </div>
+              </div>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+    <!-- End Navbar -->
+    <!-- -------- START HEADER 7 w/ text and video ------- -->
+    <header class="bg-gradient-dark">
+      <div class="page-header min-vh-80"
+        style="background-image: url('./assets/img/font.avif');">
+        <span class="mask bg-gradient-info opacity-8"></span>
+        <div class="container">
+          <div class="row justify-content-center">
+            <div class="col-lg-8 text-center mx-auto my-auto">
+              <div>
+
+              </div>
+              <h6 class="text-white mb-2 mt-6">Find us</h6>
+              <div class="d-flex justify-content-center">
+                <a
+                  href="https://www.facebook.com/Avivandolafecentral?locale=es_LA">
+                  <i class="fab fa-facebook me-4"
+                    style="color: #1877f2; font-size: 2rem;"></i>
+                </a>
+                <a href="https://www.instagram.com/avivandolafeoficial/">
+                  <i class="fab fa-instagram me-4"
+                    style="color: #e4405f; font-size: 2rem;"></i>
+                </a>
+                <a href="https://www.avivandolafe.org/">
+                  <i class="fab fa-google-plus me-4"
+                    style="color: #db4437; font-size: 2rem;"></i>
+                </a>
+              </div>
+            </div>
+
+          </div>
+        </div>
+        <div class="position-absolute w-100 z-index-1 bottom-0">
+          <svg class="waves" xmlns="http://www.w3.org/2000/svg"
+            xmlns:xlink="http://www.w3.org/1999/xlink" viewBox="0 24 150 40"
+            preserveAspectRatio="none" shape-rendering="auto">
+            <defs>
+              <path id="gentle-wave"
+                d="M-160 44c30 0 58-18 88-18s 58 18 88 18 58-18 88-18 58 18 88 18 v44h-352z" />
+            </defs>
+            <g class="moving-waves">
+              <use xlink:href="#gentle-wave" x="48" y="-1"
+                fill="rgba(255,255,255,0.40" />
+              <use xlink:href="#gentle-wave" x="48" y="3"
+                fill="rgba(255,255,255,0.35)" />
+              <use xlink:href="#gentle-wave" x="48" y="5"
+                fill="rgba(255,255,255,0.25)" />
+              <use xlink:href="#gentle-wave" x="48" y="8"
+                fill="rgba(255,255,255,0.20)" />
+              <use xlink:href="#gentle-wave" x="48" y="13"
+                fill="rgba(255,255,255,0.15)" />
+              <use xlink:href="#gentle-wave" x="48" y="16"
+                fill="rgba(255,255,255,1" />
+            </g>
+          </svg>
+        </div>
+      </div>
+    </header>
+    <!-- -------- END HEADER 7 w/ text and video ------- -->
+    <!-- Section with four info areas left & one card right with image and waves -->
+    <section class="py-5">
+  <div class="container">
+    <div class="row align-items-center">
+      <div id="video-container" class="col-12">
+        <div class="embed-responsive embed-responsive-16by9">
+          <iframe class="embed-responsive-item" src="https://tv91.hostingnuclear.com:2020/VideoPlayer/avivandolafe?" allowfullscreen></iframe>
+        </div>
+      </div>
+    </div>
+  </div>
+</section>
+
+<style>
+  .embed-responsive {
+    position: relative;
+    display: block;
+    width: 100%;
+    padding: 0;
+    overflow: hidden;
+  }
+  
+  .embed-responsive::before {
+    display: block;
+    content: "";
+    padding-top: 56.25%; /* Proporción 16:9 */
+  }
+  
+  .embed-responsive .embed-responsive-item {
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    border: 0;
+  }
+</style>
+
+
+    <!-- END Section with four info areas left & one card right with image and waves -->
+    <!-- -------- START Features w/ pattern background & stats & rocket -------- -->
+    <section class="pt-sm-8 pb-5 position-relative bg-gradient-dark">
+
+
+    </section>
+
+
+    <footer class="footer pt-5 mt-5">
+      <hr class="horizontal dark mb-5">
+      <div class="container">
+        <div class="row">
+          <div class="col-md-3 mb-4 ms-auto">
+            <div>
+              <h6 class="text-gradient text-primary font-weight-bolder">IGLESIA
+                AVIVANDO LA FE</h6>
+            </div>
+          </div>
+          <div class="col-12">
+            <div class="text-center">
+              <p class="my-4 text-sm">All rights reserved. Copyright ©
+                <script>document.write(new Date().getFullYear())</script>
+                Avivando la fe.</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </footer>
+    <script src="./assets/js/core/popper.min.js"
+      type="text/javascript"></script>
+    <script src="./assets/js/core/bootstrap.min.js"
+      type="text/javascript"></script>
+    <script src="./assets/js/plugins/perfect-scrollbar.min.js"></script>
+    <script src="./assets/js/plugins/countup.min.js"></script>
+    <script src="./assets/js/plugins/parallax.min.js"></script>
+    <script
+      src="https://maps.googleapis.com/maps/api/js?key=AIzaSyDTTfWur0PDbZWPr7Pmq8K3jiDp0_xUziI"></script>
+    <script src="./assets/js/soft-design-system.min.js?v=1.0.9"
+      type="text/javascript"></script>
+  </body>
+</html>
